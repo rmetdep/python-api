709a007526d6491ade06c3b492435331cb19c4fc
rmetdep <r0893183@student.thomasmore.be>
2022-11-17 15:18:22 +0100

    changed from internal data to database

--- api/app/main.py
+++ api/app/main.py
@@ -29,6 +29,10 @@ def read_root():
 # test
 @app.get("/test")
 def test():
+    return {"test": "no tests active"}
+
+@app.get("/driver") # get a rondom driver back
+async def get_driver():
     file = []
     cursor.execute("SELECT driverName FROM api.driver")
     for x in cursor:
@@ -36,15 +40,18 @@ def test():
     result = file[randint(1, len(file))-1]
     # remove [] brackets in result
     result = str(result).replace("(", "").replace(")", "").replace(",", "").replace("'", "")
-    return {"query": result}
-
-@app.get("/driver") # get a rondom driver back
-async def get_driver():
-    return {"driver": drivers[randint(1, 19)-1]}
+    return {"driver": result}
 
 @app.get("/team")   # get a random team back
 async def get_team():
-    return {"team": teams[randint(1, 9)-1]}
+    file = []
+    cursor.execute("SELECT teamName FROM api.team")
+    for x in cursor:
+        file.append(x)
+    result = file[randint(1, len(file))-1]
+    # remove [] brackets in result
+    result = str(result).replace("(", "").replace(")", "").replace(",", "").replace("'", "")
+    return {"team": result}
 
 @app.post("/addciruit") # lookup a drivers stats by name
 async def add_circuit(circuit: str = Query(...)):
